20fcf92607e095739431a7ac623d19b0022da8c7
tr1fker <cfjhxhkxck@gmail.com>
2025-08-21 18:07:01 +0300

    Added static currency rates data

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,5 +1,27 @@
 public class CurrencyConverter {
     public static void main(String[] args) {
+        CurrencyRatesManager currencyRatesManager = new CurrencyRatesManager();
+        currencyRatesManager.addCurrencyRate("USD", "Доллар", 0.8582d, "EUR", "Евро");
+        currencyRatesManager.addCurrencyRate("USD", "Доллар", 2.9738d, "BYN", "Бел. рубль");
+        currencyRatesManager.addCurrencyRate("USD", "Доллар", 80.4609d, "RUB", "Рос. рубль");
+        currencyRatesManager.addCurrencyRate("USD", "Доллар", 3.6493d, "PLN", "Польский злотый");
+        currencyRatesManager.addCurrencyRate("USD", "Доллар", 7.2113d, "CNY", "Китайский юань");
+
+        currencyRatesManager.addCurrencyRate("EUR", "Евро", 3.4652d,"BYN", "Бел. рубль");
+        currencyRatesManager.addCurrencyRate("EUR", "Евро", 93.7554d,"RUB", "Рос. рубль");
+        currencyRatesManager.addCurrencyRate("EUR", "Евро", 4.2522d,"PLN", "Польский злотый");
+        currencyRatesManager.addCurrencyRate("EUR", "Евро", 8.4026d, "CNY", "Китайский юань");
+
+        currencyRatesManager.addCurrencyRate("BYN", "Бел. рубль", 27.0563d, "RUB", "Рос. рубль");
+        currencyRatesManager.addCurrencyRate("BYN", "Бел. рубль", 1.2271d, "PLN", "Польский злотый");
+        currencyRatesManager.addCurrencyRate("BYN", "Бел. рубль", 2.4248d, "CNY", "Китайский юань");
+
+        currencyRatesManager.addCurrencyRate("RUB", "Рос. рубль", 0.0454d, "PLN", "Польский злотый");
+        currencyRatesManager.addCurrencyRate("RUB", "Рос. рубль", 0.0897d, "CNY", "Китайский юань");
+
+        currencyRatesManager.addCurrencyRate("PLN", "Польский злотый", 1.9761d, "CNY", "Китайский юань");
+        //USD-EUR-BYN-RUB-PLN
+
         
     }
 }
